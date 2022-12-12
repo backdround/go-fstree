4a3e45fc0949de23c9763b12b0945660f2e5500b
Vlad Chepaykyn <backdround@yandex.ru>
2022-12-13 01:09:08 +0300

    feature: add fstree root level tests

---
+++ fstree_test.go
@@ -0,0 +1,158 @@
+package fstree_test
+
+import (
+	"os"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/lithammer/dedent"
+	"github.com/stretchr/testify/require"
+
+	"github.com/backdround/go-fstree"
+)
+
+////////////////////////////////////////////////////////////
+// Utility functions
+
+func assertNoError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func prepareYaml(data string) string {
+	data = dedent.Dedent(data)
+	data = strings.ReplaceAll(data, "\t", "  ")
+	return data
+}
+
+func createRoot() (rootPath string, clean func()) {
+	rootPath, err := os.MkdirTemp("", "go-fstree-test-*.d")
+	assertNoError(err)
+
+	clean = func() {
+		err := os.RemoveAll(rootPath)
+		assertNoError(err)
+	}
+
+	return
+}
+
+////////////////////////////////////////////////////////////
+// Tests
+
+func TestEmptyRoot(t *testing.T) {
+	yamlData :=`file.txt: {type: "file", data: "data"}`
+
+	err := fstree.MakeOverOSFS("", yamlData)
+	require.Error(t, err)
+}
+
+func TestFileCreation(t *testing.T) {
+	t.Run("Empty", func(t *testing.T) {
+		yamlData :=`
+			file.txt:
+				type: file
+		`
+		yamlData = prepareYaml(yamlData)
+
+		root, clean := createRoot()
+		defer clean()
+
+		err := fstree.MakeOverOSFS(root, yamlData)
+		require.NoError(t, err)
+
+		filePath := path.Join(root, "file.txt")
+		data, err := os.ReadFile(filePath)
+		require.NoError(t, err)
+		require.Empty(t, data)
+	})
+
+	t.Run("WithData", func(t *testing.T) {
+		yamlData :=`
+			file.txt:
+				type: file
+				data: some data
+		`
+		yamlData = prepareYaml(yamlData)
+
+		root, clean := createRoot()
+		defer clean()
+
+		err := fstree.MakeOverOSFS(root, yamlData)
+		require.NoError(t, err)
+
+		filePath := path.Join(root, "file.txt")
+		data, err := os.ReadFile(filePath)
+		require.NoError(t, err)
+		require.Equal(t, "some data", string(data))
+	})
+
+	t.Run("InvalidData", func(t *testing.T) {
+		yamlData :=`
+			file.txt:
+				type: file
+				data:
+					var: dictionary value
+		`
+		yamlData = prepareYaml(yamlData)
+
+		root, clean := createRoot()
+		defer clean()
+
+		err := fstree.MakeOverOSFS(root, yamlData)
+		require.Error(t, err)
+	})
+}
+
+func TestLinkCreation(t *testing.T) {
+	t.Run("Valid", func(t *testing.T) {
+		yamlData :=`
+			link:
+				type: link
+				path: "./destination"
+		`
+		yamlData = prepareYaml(yamlData)
+
+		root, clean := createRoot()
+		defer clean()
+
+		err := fstree.MakeOverOSFS(root, yamlData)
+		require.NoError(t, err)
+
+		linkPath := path.Join(root, "link")
+		linkDestination, err := os.Readlink(linkPath)
+		require.NoError(t, err)
+		require.Equal(t, "./destination", linkDestination)
+	})
+
+	t.Run("Invalid", func(t *testing.T) {
+		yamlData :=`
+			link:
+				type: link
+		`
+		yamlData = prepareYaml(yamlData)
+
+		root, clean := createRoot()
+		defer clean()
+
+		err := fstree.MakeOverOSFS(root, yamlData)
+		require.Error(t, err)
+	})
+}
+
+func TestDirectoryCreation(t *testing.T) {
+	yamlData :=`directory:`
+
+	root, clean := createRoot()
+	defer clean()
+
+	err := fstree.MakeOverOSFS(root, yamlData)
+	require.NoError(t, err)
+
+	directoryPath := path.Join(root, "directory")
+	pathInfo, err := os.Lstat(directoryPath)
+	require.NoError(t, err)
+	require.True(t, pathInfo.IsDir())
+}
