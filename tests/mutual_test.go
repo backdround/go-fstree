a13765bd2b0a62e29f03d2a576fab40dd68fcc86
Vlad Chepaykyn <backdround@yandex.ru>
2022-12-15 21:40:59 +0300

    feature: add mutual test

---
+++ tests/mutual_test.go
@@ -0,0 +1,30 @@
+package fstree_test
+
+import (
+	"testing"
+
+	"github.com/backdround/go-fstree"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMutual(t *testing.T) {
+	root, clean := createRoot()
+	defer clean()
+
+	yamlData := prepareYaml(`
+		new-directory:
+			file.txt:
+				type: file
+			link1:
+				type: link
+				path: ./file.txt
+			subdirectory:
+	`)
+
+	err := fstree.MakeOverOSFS(root, yamlData)
+	require.NoError(t, err)
+
+	difference, err := fstree.CheckOverOSFS(root, yamlData)
+	require.NoError(t, err)
+	require.Nil(t, difference)
+}
