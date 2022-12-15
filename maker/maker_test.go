995b4e0c0983ab76930fa7b12c63106d1f991574
Vlad Chepaykyn <backdround@yandex.ru>
2022-12-15 21:18:05 +0300

    refactor: make fstree tests

--- maker/maker_test.go
+++ maker/maker_test.go
@@ -11,6 +11,9 @@ import (
 	"github.com/backdround/go-fstree/entries"
 )
 
+////////////////////////////////////////////////////////////
+// Utility functions
+
 func assertNoError(err error) {
 	if err != nil {
 		panic(err)
@@ -36,20 +39,100 @@ func GetOsMaker() Maker {
 	return maker
 }
 
-func TestFileCreationCornerCases(t *testing.T) {
-	t.Run("ErrorOnAnotherFileAlreadyExists", func(t *testing.T) {
+////////////////////////////////////////////////////////////
+// Perform test functions
+
+func performMake(rootPath string, internalEntries ...entries.Entry) (error) {
+	newTree := entries.DirectoryEntry{
+		Name: "./",
+		Entries: internalEntries,
+	}
+
+	maker := Maker{osfs.OsFS{}}
+	return maker.Make(rootPath, newTree)
+}
+
+////////////////////////////////////////////////////////////
+// Asserts functions
+
+func requireFile(t *testing.T, basePath string, name string, data string) {
+	t.Helper()
+
+	filePath := path.Join(basePath, name)
+	realData, err :=  os.ReadFile(filePath)
+	require.NoError(t, err)
+	require.Equal(t, data, string(realData))
+}
+
+func requireLink(t *testing.T, basePath string, name string,
+	destination string) {
+	t.Helper()
+
+	linkPath := path.Join(basePath, name)
+	realDestination, err :=  os.Readlink(linkPath)
+	require.NoError(t, err)
+
+	match, err := path.Match(destination, realDestination)
+	require.NoError(t, err)
+	require.True(t, match)
+}
+
+func requireDirectory(t *testing.T, basePath string, name string) {
+	t.Helper()
+
+	directoryPath := path.Join(basePath, name)
+	info, err := os.Lstat(directoryPath)
+	require.NoError(t, err)
+	require.True(t, info.IsDir())
+}
+
+////////////////////////////////////////////////////////////
+// Tests
+
+func TestEmptyYamlData(t *testing.T) {
+	err := performMake("")
+	require.Error(t, err)
+}
+
+func TestEmptyRootPath(t *testing.T) {
+	rootPath, clean := createRoot()
+	defer clean()
+
+	err := performMake(rootPath)
+	require.NoError(t, err)
+}
+
+func TestRootDoesntExist(t *testing.T) {
+	rootPath, clean := createRoot()
+	clean()
+
+	err := performMake(rootPath)
+	require.NoError(t, err)
+	requireDirectory(t, rootPath, ".")
+
+	clean()
+}
+
+func TestFile(t *testing.T) {
+	t.Run("SuccessOnNewFile", func(t *testing.T) {
 		rootPath, clean := createRoot()
 		defer clean()
 
-		rootEntry := entries.DirectoryEntry{
-			Name: ".",
-			Entries: []entries.Entry{
-				entries.FileEntry{
-					Name: "file.txt",
-					Data: []byte("some data"),
-				},
-			},
-		}
+		// Tests
+		err := performMake(rootPath,
+			entries.FileEntry{
+				Name: "file.txt",
+				Data: []byte("some data"),
+		})
+
+		// Asserts
+		require.NoError(t, err)
+		requireFile(t, rootPath, "file.txt", "some data")
+	})
+
+	t.Run("ErrorOnAnotherFileAlreadyExists", func(t *testing.T) {
+		rootPath, clean := createRoot()
+		defer clean()
 
 		// Creates a file with another data
 		existingFilePath := path.Join(rootPath, "file.txt")
@@ -57,7 +140,13 @@ func TestFileCreationCornerCases(t *testing.T) {
 		assertNoError(err)
 
 		// Tests
-		err = GetOsMaker().Make(rootPath, rootEntry)
+		err = performMake(rootPath,
+			entries.FileEntry{
+				Name: "file.txt",
+				Data: []byte("some data"),
+		})
+
+		// Asserts
 		require.Error(t, err)
 		require.Contains(t, err.Error(), "already exists")
 	})
@@ -66,49 +155,58 @@ func TestFileCreationCornerCases(t *testing.T) {
 		rootPath, clean := createRoot()
 		defer clean()
 
-		rootEntry := entries.DirectoryEntry{
-			Name: ".",
-			Entries: []entries.Entry{
-				entries.FileEntry{
-					Name: "file.txt",
-					Data: []byte("some data"),
-				},
-			},
-		}
-
 		// Creates a file with the same data
 		existingFilePath := path.Join(rootPath, "file.txt")
 		err := os.WriteFile(existingFilePath, []byte("some data"), 0644)
 		assertNoError(err)
 
 		// Tests
-		err = GetOsMaker().Make(rootPath, rootEntry)
+		err = performMake(rootPath,
+			entries.FileEntry{
+				Name: "file.txt",
+				Data: []byte("some data"),
+		})
+
+		// Asserts
 		require.NoError(t, err)
+		requireFile(t, rootPath, "file.txt", "some data")
 	})
 }
 
-func TestLinkCreationCornerCases(t *testing.T) {
+func TestLink(t *testing.T) {
+	t.Run("SuccessOnNewlink", func(t *testing.T) {
+		rootPath, clean := createRoot()
+		defer clean()
+
+		// Tests
+		err := performMake(rootPath,
+			entries.LinkEntry{
+				Name: "link1",
+				Path: "./file.txt",
+		})
+
+		// Asserts
+		require.NoError(t, err)
+		requireLink(t, rootPath, "link1", "./file.txt")
+	})
+
 	t.Run("ErrorOnFileAlreadyExists", func(t *testing.T) {
 		rootPath, clean := createRoot()
 		defer clean()
 
-		rootEntry := entries.DirectoryEntry{
-			Name: ".",
-			Entries: []entries.Entry{
-				entries.LinkEntry{
-					Name: "link1",
-					Path: "./file.txt",
-				},
-			},
-		}
-
-		// Creates a file with the same path
+		// Creates a file
 		existingFilePath := path.Join(rootPath, "link1")
 		err := os.WriteFile(existingFilePath, []byte("data"), 0644)
 		assertNoError(err)
 
 		// Tests
-		err = GetOsMaker().Make(rootPath, rootEntry)
+		err = performMake(rootPath,
+			entries.LinkEntry{
+				Name: "link1",
+				Path: "./file.txt",
+		})
+
+		// Asserts
 		require.Error(t, err)
 		require.Contains(t, err.Error(), "already exists")
 	})
@@ -117,23 +215,19 @@ func TestLinkCreationCornerCases(t *testing.T) {
 		rootPath, clean := createRoot()
 		defer clean()
 
-		rootEntry := entries.DirectoryEntry{
-			Name: ".",
-			Entries: []entries.Entry{
-				entries.LinkEntry{
-					Name: "link1",
-					Path: "./file.txt",
-				},
-			},
-		}
-
 		// Creates a link with another destination
 		existingLinkPath := path.Join(rootPath, "link1")
 		err := os.Symlink("./another-file.txt", existingLinkPath)
 		assertNoError(err)
 
 		// Tests
-		err = GetOsMaker().Make(rootPath, rootEntry)
+		err = performMake(rootPath,
+			entries.LinkEntry{
+				Name: "link1",
+				Path: "./file.txt",
+		})
+
+		// Asserts
 		require.Error(t, err)
 		require.Contains(t, err.Error(), "already exists")
 	})
@@ -142,41 +236,42 @@ func TestLinkCreationCornerCases(t *testing.T) {
 		rootPath, clean := createRoot()
 		defer clean()
 
-		rootEntry := entries.DirectoryEntry{
-			Name: ".",
-			Entries: []entries.Entry{
-				entries.LinkEntry{
-					Name: "link1",
-					Path: "./file.txt",
-				},
-			},
-		}
-
 		// Creates a link with the same destination
 		existingLinkPath := path.Join(rootPath, "link1")
 		err := os.Symlink("./file.txt", existingLinkPath)
 		assertNoError(err)
 
 		// Tests
-		err = GetOsMaker().Make(rootPath, rootEntry)
+		err = performMake(rootPath,
+			entries.LinkEntry{
+				Name: "link1",
+				Path: "./file.txt",
+		})
+
+		// Asserts
 		require.NoError(t, err)
 	})
 }
 
-func TestDirectoryCreationCornerCases(t *testing.T) {
-	t.Run("ErrorOnFileAlreadyExists", func(t *testing.T) {
+func TestDirectory(t *testing.T) {
+	t.Run("SuccessOnNewDirectory", func(t *testing.T) {
 		rootPath, clean := createRoot()
 		defer clean()
 
-		rootEntry := entries.DirectoryEntry{
-			Name: ".",
-			Entries: []entries.Entry{
-				entries.DirectoryEntry{
-					Name:    "new-directory",
-					Entries: []entries.Entry{},
-				},
-			},
-		}
+		// Tests
+		err := performMake(rootPath,
+			entries.DirectoryEntry{
+				Name: "new-directory",
+				Entries: []entries.Entry{},
+		})
+
+		// Asserts
+		require.NoError(t, err)
+	})
+
+	t.Run("ErrorOnFileAlreadyExists", func(t *testing.T) {
+		rootPath, clean := createRoot()
+		defer clean()
 
 		// Creates a file with the same path
 		existingFilePath := path.Join(rootPath, "new-directory")
@@ -184,7 +279,13 @@ func TestDirectoryCreationCornerCases(t *testing.T) {
 		assertNoError(err)
 
 		// Tests
-		err = GetOsMaker().Make(rootPath, rootEntry)
+		err = performMake(rootPath,
+			entries.DirectoryEntry{
+				Name: "new-directory",
+				Entries: []entries.Entry{},
+		})
+
+		// Asserts
 		require.Error(t, err)
 		require.Contains(t, err.Error(), "already exists")
 	})
@@ -193,23 +294,53 @@ func TestDirectoryCreationCornerCases(t *testing.T) {
 		rootPath, clean := createRoot()
 		defer clean()
 
-		rootEntry := entries.DirectoryEntry{
-			Name: ".",
-			Entries: []entries.Entry{
-				entries.DirectoryEntry{
-					Name:    "new-directory",
-					Entries: []entries.Entry{},
-				},
-			},
-		}
-
 		// Creates a directory with the same path
 		existingDirectoryPath := path.Join(rootPath, "new-directory")
 		err := os.Mkdir(existingDirectoryPath, 0755)
 		assertNoError(err)
 
 		// Tests
-		err = GetOsMaker().Make(rootPath, rootEntry)
+		err = performMake(rootPath,
+			entries.DirectoryEntry{
+				Name: "new-directory",
+				Entries: []entries.Entry{},
+		})
+
+		// Asserts
 		require.NoError(t, err)
 	})
+}
+
+func TestSubDirectory(t *testing.T) {
+	rootPath, clean := createRoot()
+	defer clean()
+
+	// Tests
+	err := performMake(rootPath,
+		entries.DirectoryEntry{
+			Name: "sub-directory",
+			Entries: []entries.Entry{
+				entries.FileEntry{
+					Name: "file.txt",
+					Data: []byte("some data"),
+				},
+				entries.LinkEntry{
+					Name: "link1",
+					Path: "./file.txt",
+				},
+				entries.DirectoryEntry{
+					Name: "more-sub-directory",
+					Entries: []entries.Entry{},
+				},
+			},
+	})
+
+	// Asserts
+	require.NoError(t, err)
+	requireDirectory(t, rootPath, "sub-directory")
+
+	subdirectoryPath := path.Join(rootPath, "sub-directory")
+	requireFile(t, subdirectoryPath, "file.txt", "some data")
+	requireLink(t, subdirectoryPath, "link1", "./file.txt")
+	requireDirectory(t, subdirectoryPath, "more-sub-directory")
 }
---
+++ tests/make_test.go
@@ -0,0 +1,145 @@
+package fstree_test
+
+import (
+	"os"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/lithammer/dedent"
+	"github.com/stretchr/testify/require"
+
+	"github.com/backdround/go-fstree"
+)
+
+////////////////////////////////////////////////////////////
+// Utility functions
+
+func assertNoError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func prepareYaml(data string) string {
+	data = dedent.Dedent(data)
+	data = strings.ReplaceAll(data, "\t", "  ")
+	return data
+}
+
+func createRoot() (rootPath string, clean func()) {
+	rootPath, err := os.MkdirTemp("", "go-fstree-test-*.d")
+	assertNoError(err)
+
+	clean = func() {
+		err := os.RemoveAll(rootPath)
+		assertNoError(err)
+	}
+
+	return
+}
+
+
+////////////////////////////////////////////////////////////
+// Assertion functions
+
+func requireFile(t *testing.T, basePath string, name string, data string) {
+	t.Helper()
+
+	filePath := path.Join(basePath, name)
+	realData, err :=  os.ReadFile(filePath)
+	require.NoError(t, err)
+	require.Equal(t, data, string(realData))
+}
+
+func requireLink(t *testing.T, basePath string, name string,
+	destination string) {
+	t.Helper()
+
+	linkPath := path.Join(basePath, name)
+	realDestination, err :=  os.Readlink(linkPath)
+	require.NoError(t, err)
+
+	match, err := path.Match(destination, realDestination)
+	require.NoError(t, err)
+	require.True(t, match)
+}
+
+func requireDirectory(t *testing.T, basePath string, name string) {
+	t.Helper()
+
+	directoryPath := path.Join(basePath, name)
+	info, err := os.Lstat(directoryPath)
+	require.NoError(t, err)
+	require.True(t, info.IsDir())
+}
+
+////////////////////////////////////////////////////////////
+// Tests
+
+func TestMake(t *testing.T) {
+	t.Run("ErrorOnEmptyRoot", func(t *testing.T) {
+		yamlData := `file.txt: {type: "file", data: "data"}`
+
+		err := fstree.MakeOverOSFS("", yamlData)
+		require.Error(t, err)
+	})
+
+	t.Run("CreateRootOnNonexistence", func(t *testing.T) {
+		root, clean := createRoot()
+		clean()
+
+		yamlData := `file.txt: {type: "file", data: "data"}`
+		err := fstree.MakeOverOSFS(root, yamlData)
+
+		require.NoError(t, err)
+		requireDirectory(t, root, ".")
+
+		clean()
+	})
+
+	t.Run("Complex", func(t *testing.T) {
+		root, clean := createRoot()
+		defer clean()
+
+		yamlData := prepareYaml(`
+			new-directory:
+				file.txt:
+					type: file
+				link1:
+					type: link
+					path: ./file.txt
+				subdirectory:
+		`)
+
+		// Tests
+		err := fstree.MakeOverOSFS(root, yamlData)
+		require.NoError(t, err)
+
+		// Checks fs tree
+		subdirectoryPath := path.Join(root, "new-directory")
+		requireFile(t, subdirectoryPath, "file.txt", "")
+		requireLink(t, subdirectoryPath, "link1", "./file.txt")
+		requireDirectory(t, subdirectoryPath, "subdirectory")
+	})
+
+	t.Run("Idempotency", func(t *testing.T) {
+		root, clean := createRoot()
+		defer clean()
+
+		yamlData := prepareYaml(`
+			new-directory:
+				file.txt:
+					type: file
+				link:
+					type: link
+					path: ./file.txt
+				subdirectory:
+		`)
+
+		err := fstree.MakeOverOSFS(root, yamlData)
+		require.NoError(t, err)
+		err = fstree.MakeOverOSFS(root, yamlData)
+		require.NoError(t, err)
+	})
+}
